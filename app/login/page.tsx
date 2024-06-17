0f3e786d1b04c71f894ed64850a5ba5922e30c89
Ruben Droogh <rubendroogh@hotmail.nl>
2024-06-18 00:18:20 +0200

    WIP login

---
+++ app/login/page.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import {
+  TextInput,
+  PasswordInput,
+  Checkbox,
+  Anchor,
+  Paper,
+  Title,
+  Container,
+  Group,
+  Button,
+} from '@mantine/core';
+import { auth } from '@/firebase/clientApp';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { useState } from 'react';
+
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      window.location.href = '/dashboard';
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+  return (
+    <Container size={420} my={40}>
+      <Title ta="center">
+        Hallo GOLBer
+      </Title>
+    
+      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+        <TextInput label="Email" placeholder="you@golb.dev" onChange={(e) => setEmail(e.target.value)} required />
+        <PasswordInput label="Password" placeholder="Your password" onChange={(e) => setPassword(e.target.value)} required mt="md" />
+        <Group justify="space-between" mt="lg">
+          <Checkbox label="Remember me" />
+          <Anchor component="button" size="sm">
+            Forgot password?
+          </Anchor>
+        </Group>
+        <Button onClick={handleLogin} fullWidth mt="xl">
+          Sign in
+        </Button>
+      </Paper>
+    </Container>
+    );
+  }
